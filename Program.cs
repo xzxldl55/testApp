c940b5588b30bf1661a62bd1521febfd4a71827c
Taoist <476630095@qq.com>
2018-09-10 09:54:13 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
