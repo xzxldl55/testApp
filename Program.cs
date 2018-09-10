7ae7fe32da1c87985a240c13e7e4f2c2f2d6eb07
Taoist <476630095@qq.com>
2018-09-10 09:57:51 +0800

    第一行
    add a console

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace testApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("This is my first C#");
         }
     }
 }
